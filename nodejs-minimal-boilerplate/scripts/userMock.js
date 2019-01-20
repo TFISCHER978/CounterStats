1695133a2bb4b9c814fbf3046f72c03fa3bfadcd
TFISCHER978 <tom.fischer978@gmail.com>
2019-01-20 20:17:07 +0100

    Began Create login

--- nodejs-minimal-boilerplate/scripts/userMock.js
+++ nodejs-minimal-boilerplate/scripts/userMock.js
@@ -26,11 +26,31 @@ function login(){
             user = json[getUser(email)];    //user infos
             if (user.password === password) {
                 alert("Welcome " + user.pseudo + " !"); //infos OK : log in
+
+                //go somewhere ¯\_(ツ)_/¯
+
             } else {
                 alert("Email and password doesn't match");  //email OK, password KO : alert no log
             }
         } else {
-            alert("Email : " + email + " doesn't exist");   //email KO
+            alert("Email : " + email + " doesn't exist ¯\\_(ツ)_/¯");   //email KO
+        }
+    });
+}
+
+function signup(){
+
+    $.getJSON("../mock/user.json", function(json) {
+        
+        psedo = document.getElementById("pseudo").value;
+        email = document.getElementById("email").value;
+        password = document.getElementById("password").value;
+        confirmPass = document.getElementById("confirm").value;
+
+        if (!contains(json,"email",email)) {
+            // add to json
+        } else {
+            alert("Email : " + email + " already in use ¯\\_(ツ)_/¯");   //email already in base
         }
     });
 }
