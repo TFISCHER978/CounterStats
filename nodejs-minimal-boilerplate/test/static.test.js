fe2e8970a16646edea94ee415295a8df401592a3
IUTNICE\ft714367 <tom.fischer978@gmail.com>
2019-01-22 10:02:19 +0100

    Add TU on css files

--- nodejs-minimal-boilerplate/test/static.test.js
+++ nodejs-minimal-boilerplate/test/static.test.js
@@ -7,8 +7,16 @@ describe("Test the root path", () => {
     expect(response.statusCode).toBe(200);
     expect(response.header["content-type"]).toBe("text/html; charset=utf-8");
   });
-  test("It should serve css file", async () => {
-    const response = await request(app).get("/static/style.css");
+  test("It should serve home.css file", async () => {
+    const response = await request(app).get("/static/home.css");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should serve login.css file", async () => {
+    const response = await request(app).get("/static/login.css");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should serve signup.css file", async () => {
+    const response = await request(app).get("/static/signup.css");
     expect(response.statusCode).toBe(200);
   });
 });
